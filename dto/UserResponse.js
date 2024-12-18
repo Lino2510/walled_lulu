151a8b83e88cc4abbf3379536256449ac450672b
lulu <luluulmawarnafis@outlook.com>
2024-12-18 15:14:51 +0700

    update localhostt

--- dto/UserResponse.js
+++ dto/UserResponse.js
@@ -1,13 +1,12 @@
-// //DTO
-// class UserResponse{
-//     constructor(user){
-//         this.id =user.id
-//         this.username = user.username;
-//         this.fullname = user.fullname;
-//         this.email = user.email;
-//         this.balance = user.balance;
-        
-//     }
-// }
+//DTO
+class UserResponse {
+  constructor(user) {
+    this.id = user.id;
+    this.username = user.username;
+    this.fullname = user.fullname;
+    this.email = user.email;
+    this.balance = user.balance;
+  }
+}
 
-// module.exports = {UserResponse};
+module.exports = { UserResponse };
