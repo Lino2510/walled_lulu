5e9dfe49eec6e099cec271eb74b54b04be41b87d
lulu <luluulmawarnafis@outlook.com>
2024-12-18 15:22:23 +0700

    localhost fixing again

--- dto/UserResponse.js
+++ dto/UserResponse.js
@@ -1,11 +1,16 @@
-//DTO
 class UserResponse {
   constructor(user) {
     this.id = user.id;
     this.username = user.username;
     this.fullname = user.fullname;
     this.email = user.email;
-    this.balance = user.balance;
+    this.avatar_url = user.avatar_url;
+    if (user.wallet) {
+      this.wallet = {
+        account_number: user.wallet.account_number,
+        balance: user.wallet.balance,
+      };
+    }
   }
 }
 
